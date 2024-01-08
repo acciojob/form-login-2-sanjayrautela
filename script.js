83620cbcd9d0ac27c395c57cce30d8b80153e7d6
Sanjay Rautela <140192191+sanjayrautela@users.noreply.github.com>
2024-01-08 18:19:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+    function submitForm() {
+      var firstName = document.getElementById("firstName").value;
+      var lastName = document.getElementById("lastName").value;
+      var phoneNumber = document.getElementById("phoneNumber").value;
+      var email = document.getElementById("email").value;
+
+      var result = "First Name: " + firstName + "\nLast Name: " + lastName + "\nPhone Number: " + phoneNumber + "\nEmail ID: " + email;
+
+      alert(result);
+    }
+
